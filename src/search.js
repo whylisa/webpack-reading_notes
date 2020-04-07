c8d15ed027cf1c36378c24f5f2bc0a8cd23ea0fb
haoyue.wang <843655240@qq.com>
2020-04-07 19:09:06 +0800

    react配置及使用

--- src/search.js
+++ src/search.js
@@ -1,1 +1,13 @@
-document.write('hh')
+'use strict'
+ import React from 'react'
+ import ReactDOM from 'react-dom'
+ class Search extends React.Component {
+	 render () {
+		 return <div> search</div>
+	 }
+	 
+ }
+ ReactDOM.render(
+   <Search />,
+	 document.getElementById('root')
+ )
--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,6 @@ module.exports = {
 				use: 'babel-loader'
 			}
 		]
-	}
+	},
 	mode: 'production'
 }
